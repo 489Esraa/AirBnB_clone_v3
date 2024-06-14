8a88f2c9847123c8296e2db611208816b474b453
DarkWeirdo <sonic_unleashed@live.com>
2024-06-14 03:45:47 +0300

    state task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,17 +2,22 @@
 
 """States route """
 
-from flask import jsonify, request
+from flask import jsonify, request, make_response
 from models import storage
 from models.state import State
 from api.v1.views import app_views
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
-def states():
-    """Retrieves the list of all State objects"""
-    states = storage.all(State)
-    return jsonify([state.to_dict() for state in states.values()])
+def get_states():
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
 
 @app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
@@ -50,15 +55,18 @@ def create_state():
 
 
 @app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
-def update_state(state_id):
+def put_state(state_id):
     """Updates a State object"""
     state = storage.get(State, state_id)
     if state is None:
         return jsonify({"error": "Not found"}), 404
     if not request.get_json():
         return jsonify({"error": "Not a JSON"}), 400
-    for key, value in request.get_json().items():
-        if key not in ["id", "created_at", "updated_at"]:
+    ignore = ['id', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
             setattr(state, key, value)
     storage.save()
-    return jsonify(state.to_dict())
+    return make_response(jsonify(state.to_dict()), 200)
