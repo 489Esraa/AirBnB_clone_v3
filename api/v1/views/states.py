dc69c1b6cba8abbd5854df6cfb4ce85294c5f0d4
DarkWeirdo <sonic_unleashed@live.com>
2024-06-14 03:48:11 +0300

    Revert "state task"
    This reverts commit 8a88f2c9847123c8296e2db611208816b474b453.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,22 +2,17 @@
 
 """States route """
 
-from flask import jsonify, request, make_response
+from flask import jsonify, request
 from models import storage
 from models.state import State
 from api.v1.views import app_views
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
-def get_states():
-    """
-    Retrieves the list of all State objects
-    """
-    all_states = storage.all(State).values()
-    list_states = []
-    for state in all_states:
-        list_states.append(state.to_dict())
-    return jsonify(list_states)
+def states():
+    """Retrieves the list of all State objects"""
+    states = storage.all(State)
+    return jsonify([state.to_dict() for state in states.values()])
 
 
 @app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
@@ -55,18 +50,15 @@ def create_state():
 
 
 @app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
-def put_state(state_id):
+def update_state(state_id):
     """Updates a State object"""
     state = storage.get(State, state_id)
     if state is None:
         return jsonify({"error": "Not found"}), 404
     if not request.get_json():
         return jsonify({"error": "Not a JSON"}), 400
-    ignore = ['id', 'created_at', 'updated_at']
-
-    data = request.get_json()
-    for key, value in data.items():
-        if key not in ignore:
+    for key, value in request.get_json().items():
+        if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, value)
     storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    return jsonify(state.to_dict())
